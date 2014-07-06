ecc31f174ba24184c407cb03193b5b88ea0e80b3
IbaiLUX <Ibailux@gmail.com>
2014-07-06 23:54:49 +0200

    Añadido movimiento PJ
    Añadida mecánica de movimiento del jugador y un efecto de sombra para
    simular "paneo" y "tileo".
    Añadida un prueba de bala.

---
+++ Assets/Scripts/Bala_Movimiento.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// Bala_ movimiento.
+/// Se encarga de mover la bala al ser instanciada.
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class Bala_Movimiento : MonoBehaviour {
+	//La velocidad de la bala.
+	public float miVelocidad;
+	//Al instanciarse.
+	void Start () {
+		//Añado velocidad 
+		rigidbody2D.velocity = new Vector2(0,1)*miVelocidad;
+	}
+}
---
+++ Assets/Scripts/PJ_EfectoAcelerador.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// P j_ acelerador.
+/// Este script se encarga de mostrar y ocultar el efecto de llama al acelerar.
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class PJ_EfectoAcelerador : MonoBehaviour {
+	//Spite del efecto.
+	public SpriteRenderer llama;
+
+	void Update () {
+		//Si la velocidad es diferente a cero...
+		if (rigidbody2D.velocity != Vector2.zero) {
+			//...muestro el efecto en color rojizo.
+			llama.color = new Color (1, 0.3f, 0.3f, 1);
+		}
+		//...si es 0...
+		else {
+			//...no muestro el efecto.
+			llama.color = new Color (1, 1, 1, 0);
+		}
+	}
+}
