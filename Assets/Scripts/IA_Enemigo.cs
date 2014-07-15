9a2aa4899aae2e18c7183c8f3a84c68f8cb90d40
IbaiLUX <Ibailux@gmail.com>
2014-07-15 10:07:55 +0200

    PowerUps 1
    Añadido sistema de power ups y loteo de enemigos

--- Assets/Scripts/IA_Enemigo.cs
+++ Assets/Scripts/IA_Enemigo.cs
@@ -13,8 +13,14 @@ public class IA_Enemigo : MonoBehaviour {
 	public enum TiposMovimiento {Simple,Ola};
 	//Movimiento de esta unidad.
 	public TiposMovimiento miMovimiento = TiposMovimiento.Simple;
+	//Amplitud del movimiento en ola en X
+	public float amplitud = 4.0f;
+	//Tipos de disparos.
+	public enum TiposDisparo{Simple, Dirigido};
+	//Disparo de esta unidad.
+	public TiposDisparo miDisparo = TiposDisparo.Simple;
 	//La velocidad del enemigo.
-	public float velocidad;
+	public float velocidad = 0.5f;
 	//Prefab del disparo.
 	public GameObject disparo;
 	//Transform de donde sale el disparo.
@@ -24,17 +30,31 @@ public class IA_Enemigo : MonoBehaviour {
 	//Almacen de tiempo para el siguiente disparo.
 	private float proximoDisparo = 0.1f;
 	//Vector 2 de direccion del enemigo, por defecto negativo en eje Y.
-	public Vector2 direccion = new Vector2(0,-1);
-	
+	public Vector2 direccion;
+	//Efecto al ser destruido.
+	public GameObject explosion;
+	//Posibles premios al matarlo.
+	public GameObject premio;
+
 	void Update () {
 		//Si puedo disparar...
 		if (Time.time > proximoDisparo) {
 			//...añado tiempo para proximoDisparo.
 			proximoDisparo = Time.time + ratioDisparo;
-			//...instancio el prefab del disparo.
-			GameObject disparoEnemigo = (GameObject)Instantiate(disparo, posDisparo.position, Quaternion.identity);
-			//...renombro.
-			disparoEnemigo.name = "DisparoEnemigo";
+			//Dependiendo del tipo de disparo...
+			switch(miDisparo){
+				//...si es simple...
+				case TiposDisparo.Simple:
+					//...instancio el prefab del disparo.
+					GameObject disparoSimple = (GameObject)Instantiate(disparo, posDisparo.position, Quaternion.identity);
+					//...renombro.
+					disparoSimple.name = "DisparoEnemigo";
+				return;
+				//...si es dirigido...
+				case TiposDisparo.Dirigido:
+
+				return;
+			}
 		}
 		//Dependiendo del tipo de movimiento...
 		switch(miMovimiento){
@@ -47,11 +67,29 @@ public class IA_Enemigo : MonoBehaviour {
 			return;
 			//...si es en forma de ola...
 			case TiposMovimiento.Ola:
-				//...para moverse en forma de ola direccion en X varia de -1.5 a 1.5.
-				direccion = new Vector2((Mathf.PingPong(Time.time*velocidad, 3) - 1.5f),-1);
+				//...para moverse en forma de ola direccion en X varia de amplitud a -amplitud
+				direccion = new Vector2((Mathf.PingPong(Time.time*velocidad, amplitud) - (amplitud/2)),-velocidad);
 				//...añado velocidad.
 				rigidbody2D.velocity = direccion * velocidad;
 			return;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		//Si el objeto que dispara el trigger es la bala del jugador...
+		if (other.tag == "BalaJugador") {
+			//...destruyo la bala.
+			Destroy(other.gameObject);
+			//...creo el efecto de explosion.
+			GameObject go = (GameObject)Instantiate(explosion, transform.position, Quaternion.identity);
+			//...el efecto de explosion se autodestruye en 1s(duracion de la animacion).
+			Destroy(go,1);
+			//...creo posible premio.
+			GameObject pr = (GameObject)Instantiate(premio,transform.position,Quaternion.identity);
+			//...renombro.
+			pr.name = "Premio";
+			//...destruyo enemigo.
+			Destroy(gameObject);
 		}
 	}
 }
---
+++ Assets/Scripts/PowerUP.cs
@@ -0,0 +1,154 @@
+﻿/// <summary>
+/// Power UP.
+/// Este script se encarga de asignar un color dependiendo del tipo de powerUp que sea. El tipo de powerUp sera aleatorio.
+/// El item se movera ligeramente.
+/// **NOTA**
+/// Los sprites deven estar ordenados de la siguiente forma, 0 = disparos, 1 = vida, 2 = escudo, 3 = ratio.
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class PowerUP : MonoBehaviour {
+	//Tipos de powerUp
+	public enum TiposPowerUP {Nulo,Disparo1,Disparo2,Disparo3,Disparo4,Laser,Vida,Escudo,Ratio};
+	//El power up de este item.
+	public TiposPowerUP miPowerUP = TiposPowerUP.Nulo;
+	//Referencia al script de disparo del jugador.
+	private PJ_Disparo pjD;
+	//Sprites posibles.
+	public Sprite[] sprites;
+	
+	void Start () {
+		//Recojo la referencia al script del disparo del jugador.
+		pjD = GameObject.FindGameObjectWithTag ("Player").GetComponent<PJ_Disparo> ();
+		//Le doy valor aleatorio al powerUp.
+		miPowerUP = GetRandomEnum<TiposPowerUP> ();
+		//Dependiendo del tipo de power up...
+		switch (miPowerUP) {
+			//si es Nulo...
+			case TiposPowerUP.Nulo:
+				//Lo hago transparante.
+				gameObject.GetComponent<SpriteRenderer>().color = new Color(1,1,1,0);
+				//Como es nulo lo destruyo.
+				Destroy(gameObject);
+			return;
+
+			case TiposPowerUP.Disparo1:
+				//Coloreo el item.
+				gameObject.GetComponent<SpriteRenderer>().color = new Color(1,0,0,1);
+				//Le asigno sprite.
+				gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+			return;
+
+			case TiposPowerUP.Disparo2:
+				//Coloreo el item.
+				gameObject.GetComponent<SpriteRenderer>().color = new Color(0,1,0,1);
+				//Le asigno sprite.
+				gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+			return;
+
+			case TiposPowerUP.Disparo3:
+				//Coloreo el item.
+				gameObject.GetComponent<SpriteRenderer>().color = new Color(0,0,1,1);
+				//Le asigno sprite.
+				gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+			return;
+
+			case TiposPowerUP.Disparo4:
+				//Coloreo el item.
+				gameObject.GetComponent<SpriteRenderer>().color = new Color(1,1,0,1);
+				//Le asigno sprite.
+				gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+			return;
+
+			case TiposPowerUP.Laser:
+				//Coloreo el item.
+				gameObject.GetComponent<SpriteRenderer>().color = new Color(1,0,1,1);
+				//Le asigno sprite.
+				gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+			return;
+
+			case TiposPowerUP.Vida:
+				//Coloreo el item.
+				gameObject.GetComponent<SpriteRenderer>().color = new Color(1,1,1,1);
+				//Le asigno sprite.
+				gameObject.GetComponent<SpriteRenderer>().sprite = sprites[1];
+			return;
+
+			case TiposPowerUP.Escudo:
+				//Coloreo el item.
+				gameObject.GetComponent<SpriteRenderer>().color = new Color(1,1,1,1);
+				//Le asigno sprite.
+				gameObject.GetComponent<SpriteRenderer>().sprite = sprites[2];
+			return;
+
+			case TiposPowerUP.Ratio:
+				//Coloreo el item.
+				gameObject.GetComponent<SpriteRenderer>().color = new Color(1,1,1,1);
+				//Le asigno sprite.
+				gameObject.GetComponent<SpriteRenderer>().sprite = sprites[3];
+			return;
+		}
+	}
+
+	void Update(){
+		//Muevo el item.
+		rigidbody2D.velocity = new Vector2 (Random.Range (-1,1), Random.Range (-1,1));
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		//Si el objeto que dispara el trigger es el jugador...
+		if (other.tag == "Player") {
+			//Dependiendo del powerUp...
+			switch(miPowerUP){
+				//Si es disparo 1...
+				case TiposPowerUP.Disparo1:
+					//...cambio el disparo.
+					pjD.disparoActual = PJ_Disparo.TiposDisparo.simple;
+					//...destruyo item.
+					Destroy(gameObject);
+				return;
+
+				//Si es disparo 2...
+				case TiposPowerUP.Disparo2:
+					//...cambio el disparo.
+					pjD.disparoActual = PJ_Disparo.TiposDisparo.dos;
+					//...destruyo item.
+					Destroy(gameObject);
+				return;
+
+				//Si es disparo 3...
+				case TiposPowerUP.Disparo3:
+					//...cambio el disparo.
+					pjD.disparoActual = PJ_Disparo.TiposDisparo.tres;
+					//...destruyo item.
+					Destroy(gameObject);
+				return;
+
+				//Si es disparo 4...
+				case TiposPowerUP.Disparo4:
+					//...cambio el disparo.
+					pjD.disparoActual = PJ_Disparo.TiposDisparo.cuatro;
+					//...destruyo item.
+					Destroy(gameObject);
+				return;
+
+				//Si es laser...
+				case TiposPowerUP.Laser:
+					//...cambio el disparo.
+					pjD.disparoActual = PJ_Disparo.TiposDisparo.laser;
+					//...destruyo item.
+					Destroy(gameObject);
+				return;
+			}
+		}
+	}
+
+	//Funcion que devuelve un elemento aleatorio de una enumeracion.
+	static T GetRandomEnum<T>(){
+		System.Array A = System.Enum.GetValues(typeof(T));
+		T V = (T)A.GetValue(UnityEngine.Random.Range(0,A.Length));
+		return V;
+	}
+}
